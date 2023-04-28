7dc1a9cb98ee5dfed8e61ab178536a3ad79b1f16
Taposhdas2022 <119933002+Taposhdas2022@users.noreply.github.com>
2023-04-28 13:11:28 +0600

    implement show student history check option

---
+++ src/screens/Library_student.js
@@ -0,0 +1,84 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+
+
+const Library_student=(props) =>{
+
+
+   
+    
+    return (
+        <View style={styles.container}>
+          
+          <StatusBar style="auto" />
+          
+          <Text style={{ fontSize: 40 }}> LIBRARY STUDENT</Text>
+     
+          
+
+
+
+        </View> 
+      );
+    }
+
+    
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    image: {
+      marginBottom: 40,
+    },
+    inputView: {
+      backgroundColor: "#93CCED",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+      alignItems: "center",
+    },
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    },
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+    },
+    create_account: {
+        height: 30,
+        marginBottom: 30,
+      },
+    loginBtn: {
+      width: "80%",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FFA500",
+    },
+  });
+
+  export default Library_student;
+
+  
+  
